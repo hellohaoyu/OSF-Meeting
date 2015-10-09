836a4d2738c91ef1a43ab8ffa6d8c80cc16bc792
haoyuchen1992 <hc6as@virginia.edu>
2015-10-09 12:49:48 -0400

    First commit

--- static/js/pages/index-page.js
+++
@@ -1,9 +0,0 @@
-/* Page specific javascript */
-
-$(document).ready(function(){
-	$('.prereg-button').qToggle();
-	$('.prereg-button').click(function(){
-		var target = $(this).attr('data-qToggle-target');
-		$(target).find('input').first().focus();
-	});
-});
